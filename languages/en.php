ed8babfc5db8a0476702a1789cba0485f02cf3c2
Pablo Martin <caedes@sindominio.net>
2012-11-08 07:13:31 +0000

    modify title header language english

--- languages/en.php
+++ languages/en.php
@@ -4,7 +4,7 @@ $english = array(
 
 	'anillosur_theme:landing:title' => '¡Social networks by the people and for the people!',
 	'anillosur_theme:landing:subtitle' => 'Because "the master\'s tools will never dismantle the master\'s house"',
-
+        'anillosur_theme:landing:title2' => 'https://red.anillosur.cc - https://lorea.org',
 );
 
 add_translation("en", $english);
